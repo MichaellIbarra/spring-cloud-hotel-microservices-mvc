90d2d39317df4de66b6b9cdacf0852613a2a3062
MichaellIbarra <michaell.ibarra@vallegrande.edu.pe>
2025-12-25 23:00:58 -0500

    feat(server-config-server): initialize server configuration with Maven wrapper, application properties, and basic structure

---
+++ server-config-server/src/test/java/dev/matichelo/server/configserver/ServerConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matichelo.server.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
